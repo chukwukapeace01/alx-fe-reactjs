3c77144b834f3346b7247f5edb8182633520e287
Peace Chukwuka <p.chukwuka@alustudent.com>
2026-02-15 22:47:52 +0400

    Update App.jsx with AddRecipeForm route

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Home Page */}
         <Route path="/" element={<HomePage />} />
+
+        {/* Recipe Detail Page */}
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+
+        {/* Add Recipe Form Page */}
+        <Route path="/add-recipe" element={<AddRecipeForm />} />
       </Routes>
     </Router>
   );
